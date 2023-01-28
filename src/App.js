3211d56296cc49ec992db3a25f51fb254b0c35d0
Enoc Soto <tec-enocdejesus@hotmail.com>
2023-01-28 14:36:57 -0500

    cargado todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import LoadingSpinner from "./spinner";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [Character, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleFetch = () => {
+    setIsLoading(true);
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((respose) => respose.json())
+      .then((respose) => {
+        setUsers(respose.data)
+        setIsLoading(false)
+         // Optional code to simulate delay
+         //setTimeout(() => {
+           //setUsers(respose.data);
+           //setIsLoading(false);
+         //}, 3000);
+      })
+      .catch(() => {
+         setErrorMessage("Unable to fetch user list");
+         setIsLoading(false);
+      });
+  };
+  const renderUser = (
+    <div className="userlist-container">
+      {Character.map((item, index) => (
+        <div className="user-container" key={index}>
+          <img src={item.image} alt="" />
+          <div className="userDetail">
+            <div className="name">{`${item.name}                
+                                   ${item.status}`}</div>
+            <div className="species">{item.species}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? <LoadingSpinner /> : renderUser}
+      {errorMessage && <div className="error">{errorMessage}</div>}
+      <button onClick={handleFetch} disabled={isLoading}>
+        Fetch Users
+      </button>
     </div>
   );
 }
-
-export default App;
